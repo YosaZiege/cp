d743a544ff534b0287e83c29b04ba49fcc832ffa
Yousset Chan <yousset.chan@gmail.com>
2026-06-13 14:50:45 +0100

    First Problem

---
+++ BinaryExponentiation/AModularExponentiation/main.cpp
@@ -0,0 +1,12 @@
+
+
+
+// A. Modular Exponentiation
+//given integers n and m, calculate
+//  2.pow(n).mod(m)
+// You are asked to solve the "reverse" problem. Given integers n and m, calculate
+// m . mod 2 pow (n)
+int main (int argc, char *argv[]) {
+  
+  return 0;
+}
